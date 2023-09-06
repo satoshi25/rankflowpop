0862cb8f7cd7ba6bed7c135a1c215589ea876574
satoshi25 <81741466+satoshi25@users.noreply.github.com>
2023-09-07 07:25:55 +0900

    feat: Add verify_password method and create_jwt method - requirements.txt: JWT token을 생성할 python-jose module 추가 - user.py: login 요청시 password 검증을 위한 verify_password method 추가            login 응답으로 전해줄 access_token 생성을 위한 create_jwt method 추가

--- src/service/user.py
+++ src/service/user.py
@@ -1,17 +1,41 @@
+from fastapi import Depends
+from jose import jwt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 import bcrypt
+import os
 
 from src.schema.response import UserResponse
 from src.database.repository import UserRepository
 
+load_dotenv()
+
 
 class UserService:
-    def __init__(self, user_repo: UserRepository):
+    def __init__(self, user_repo: UserRepository = Depends()):
         self.user_repo = user_repo
-        self.encode: str = "UTF-8"
+        self.encoding: str = os.getenv("HASHING_ENCODING")
+        self.secret_key: str = os.getenv("JWT_SECRET_KEY")
+        self.jwt_algorithm: str = os.getenv("JWT_ALGORITHM")
 
     def hash_password(self, password: str) -> str:
-        hashed_password: bytes = bcrypt.hashpw(password.encode(self.encode), bcrypt.gensalt())
-        return hashed_password.decode(self.encode)
+        hashed_password: bytes = bcrypt.hashpw(password.encode(self.encoding), salt=bcrypt.gensalt())
+        return hashed_password.decode(self.encoding)
+
+    def verify_password(self, password: str, hashed_password: str) -> bool:
+        return bcrypt.checkpw(password.encode(self.encoding), hashed_password.encode(self.encoding))
+
+    def create_jwt(self, username: str) -> str:
+        access_token: str = jwt.encode(
+            {
+                "sub": username,
+                "exp": datetime.now() + timedelta(days=7)
+            },
+            key=self.secret_key,
+            algorithm=self.jwt_algorithm
+        )
+
+        return access_token
 
     def create_user(self, username: str, password: str) -> UserResponse | None:
         hashed_password = self.hash_password(password=password)
