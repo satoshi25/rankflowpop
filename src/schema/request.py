4d8372e57436e3a725069014a7ecbfaf3c2982f7
satoshi25 <81741466+satoshi25@users.noreply.github.com>
2023-09-15 12:06:36 +0900

    feat: Add request validator - signup, login의 request에 validator 추가 - product create, update의 request에 validator 추가

--- src/schema/request.py
+++ src/schema/request.py
@@ -1,9 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+import re
 
 
 class UserRequest(BaseModel):
     username: str
     password: str
+
+    @validator("username")
+    def check_username(cls, v):
+        name = re.compile("(?=.*[a-z])(?!.*[~!@#$%^&*()_+=\\-/.,])[A-Za-z\\d]{4,}")
+        if not name.match(v):
+            raise ValueError("Invalid Username")
+        return v
+
+    @validator("password")
+    def check_password(cls, v):
+        ps = re.compile("(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{4,}")
+        if not ps.match(v):
+            raise ValueError("Invalid Password")
+        return v
 
 
 class ProductKeywordRequest(BaseModel):
@@ -11,3 +26,10 @@ class ProductKeywordRequest(BaseModel):
     product_name: str
     store_name: str
     keyword: str
+
+    @validator("product_name", "store_name", "keyword")
+    def check_empty(cls, v):
+        v = v.strip()
+        if v == "":
+            raise ValueError("Field Cannot Be Empty")
+        return v
